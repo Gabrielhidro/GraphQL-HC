915d25a742c1ebd8ba7572c9f3d76731ce746402
GabrielHidro <gabrielhidro.dev@gmail.com>
2021-07-22 14:56:50 -0300

    Create server

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
-console.log('Hello');
+import { createServer } from 'http';
+
+const server = createServer((request, response) => {
+  switch (request.url){
+    case '/status': {
+      response.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+      response.write(
+        JSON.stringify({
+          status: 'OK',
+        })
+      );
+      response.end();
+      return;
+    }
+    default: {
+      response.writeHead(404, 'not found');
+      response.end();
+      return;
+    }
+  }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = '127.0.0.1';
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`server listen at http://${HOSTNAME}:${PORT}.`);
+})
